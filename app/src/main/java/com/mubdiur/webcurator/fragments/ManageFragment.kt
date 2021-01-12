91dc4765c02e403f9b5c2bdd86bc648f18034814
Md. Mubdiur Rahman <mubdiur@gmail.com>
2021-01-12 09:31:08 +0600

    removed the excessive roundings

--- app/src/main/java/com/mubdiur/webcurator/fragments/ManageFragment.kt
+++ app/src/main/java/com/mubdiur/webcurator/fragments/ManageFragment.kt
@@ -1,6 +1,20 @@
 package com.mubdiur.webcurator.fragments
 
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.mubdiur.webcurator.R
 
-class ManageFragment : Fragment(R.layout.fragment_manage)
+class ManageFragment : Fragment(R.layout.fragment_manage) {
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        super.onViewCreated(view, savedInstanceState)
+        view.setOnTouchListener { _, _ -> true }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
+}
